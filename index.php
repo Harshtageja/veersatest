5d85ae4361cc092e9156ee179f0e27d746f56f9f
Harshtageja <61935490+Harshtageja@users.noreply.github.com>
2022-05-05 07:48:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
-echo "Hello world";
+   $con = "dbname=d49bepre1ggrur host=ec2-52-4-104-184.compute-1.amazonaws.com port=5432 user=d49bepre1ggrur password=3e12f3dc93d1d2c5a3af3ed6ef69c10ec477ae38288412437ebd00ef4b117c53 sslmode=require";
+
+
+   if (!$con) 
+   {
+     echo "Database connection failed.";
+   }
+   else 
+   {
+     echo "Database connection success.";
+   }
+
 ?>
